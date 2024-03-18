fbae9bf845fff536c9bec5675af6119c5ef81aa5
JeroCaller <j69m17@gmail.com>
2024-03-18 22:56:20 +0900

    예제 코드 수정.

--- about-string/src/OtherStringMethods.java
+++ about-string/src/OtherStringMethods.java
@@ -3,11 +3,26 @@ class OtherStringMethods {
         String con = "shape";
         System.out.println(con.contains("a"));
 
+        // stringSrc.startsWith(string) : 문자열이 string으로 시작하는지를 조사 후 boolean으로 반환.
         System.out.println("good".startsWith("g"));
         System.out.println("good for you".startsWith("good"));
+
+        // stringSrc.endsWith(string) : 문자열이 string으로 끝나는지를 조사 후 boolean으로 결과값 반환.
         System.out.println("good for you".endsWith("you"));
+
+        // boolean string.isEmpty() : 문자열의 길이가 0인지 확인. (string.length == 0)
         System.out.println("".isEmpty());
+
+        // toUpperCase() : 문자열의 모든 영문자들을 대문자로 변경.
+        // toLowerCase() : 문자열의 모든 영문자들을 소문자로 변경.
         System.out.println("how ARE you".toUpperCase());
         System.out.println("HOW are YOU".toLowerCase());
+
+        String strWithSpace = "  굳 아이디어  ";
+        // 문자열 양쪽 공백 제거한 결과물을 반환.
+        String strWithoutSpace = strWithSpace.trim();
+        System.out.println(strWithoutSpace);
+        System.out.println(strWithSpace.length());
+        System.out.println(strWithoutSpace.length());
     }
 }
--- about-string/src/Token.java
+++ about-string/src/Token.java
@@ -2,12 +2,16 @@ import java.util.StringTokenizer;
 
 class Token {
     public static void printTokens(StringTokenizer stTo) {
+        // hasMoreTokens() : 토큰이 아직 하나라도 남아있다면 true 반환.
         while (stTo.hasMoreTokens()) {
+            // nextToken() : 토큰을 순서대로 하나씩 반환한다. 
             System.out.println(stTo.nextToken());
         }
     }
 
     public static void main(String[] args) {
+        // 첫 번째 인자 : 분할할 문자열.
+        // 두 번째 인자 : 구분자. (생략 시 공백, 탭 또는 개행(\n)으로 기본 설정)
         StringTokenizer st = new StringTokenizer("1 2 3");
         StringTokenizer st2 = new StringTokenizer("1-2-3", "-");
 
