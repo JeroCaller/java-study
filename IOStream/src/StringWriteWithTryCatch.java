b14355854da0a0060a5ff94d3d6e300b06faeb42
JeroCaller <j69m17@gmail.com>
2024-04-01 22:28:25 +0900

    기존 예제 코드 수정.

--- IOStream/src/StringWriteWithTryCatch.java
+++ IOStream/src/StringWriteWithTryCatch.java
@@ -6,15 +6,15 @@ class StringWriteWithTryCatch {
     public static void main(String[] args) {
         OutputStreamWriter textWriter = null;
 
-        // FileWriter()는 문자열을 파일 대상으로 출력시켜 
-        // 파일에 정보를 기록할 때 사용하는 클래스.
-
         try {
             textWriter = new OutputStreamWriter(
                 new FileOutputStream("IOStream/test2.txt"), "utf-8");
             textWriter.write("안녕하세요. 텍스트 파일에 저장된 텍스트입니다.");
             // close() 메서드 호출을 하지 않아 파일을 닫는 것을 깜빡했다.
-        } catch (IOException e) {}
+        } catch (IOException e) {
+            System.out.println("입출력 관련 예외 발생!");
+            e.printStackTrace();
+        }
         finally {
             // finally 구문을 통해 파일을 무조건 닫게끔 하고 있음.
             if (textWriter != null) {
