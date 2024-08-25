2d386bf12597fdd81221feda8e473f7ab10d8f51
JeroCaller <j69m17@gmail.com>
2024-08-25 23:24:19 +0900

    코드 관리 및 jar 실습

---
+++ class-path/src/App.java
@@ -0,0 +1,12 @@
+import mylib.PrintArray;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int[] num = {1, 2, 3};
+
+        PrintArray pa = new PrintArray();
+
+        pa.printArray(num);
+
+    }
+}
